0873aa6cbcf2995da4146c008792428a6336b73b
LidermanGiraldo <lidermangiraldo@hotmail.com>
2022-09-05 18:18:01 -0500

    Endpoint Finished

--- src/routes/mobile.js
+++ src/routes/mobile.js
@@ -9,7 +9,26 @@ const purchase_orders = require('../Ordenes.json')
 
 //Endpoint Listar productos
 router.get('/producto', (request, response) => {
+    let array_result = []
+    _.each(products, (product, i) => {
+        let sku = product.sku
+        let nombre = product.nombre
+        let url = product.url
+        let marca = product.marca
+        let inventario = product.inventario
 
+        let precio = product.precio
+        let descuento = product.descuento
+        let iva = product.iva
+
+        let precio_final = precio - (precio * descuento) + (precio * iva)
+
+        if(inventario > 0){
+            const showProductsM = { sku, nombre, url, marca, precio_final}
+            array_result.push(showProductsM) 
+        }
+    })
+    response.json(array_result)
 })
 //Endpoint Consultar producto
 router.get('/producto/:sku', (request, response) => {
